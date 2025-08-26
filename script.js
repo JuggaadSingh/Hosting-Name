dd29ca47b49ec0d4ebb7c2270522bbbac8343b0f
JuggaadSingh <sandha222004@gmail.com>
2025-08-26 10:52:55 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-const username = "JuggaadSingh";
+async function getGithubData() {
+    try {
+      const response = await fetch("https://api.github.com/users/JuggaadSingh"); 
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
 
- 
+      const data = await response.json();
+
+      document.getElementById("username").innerHTML = data.login;
+      document.getElementById("repos").innerHTML = data.public_repos;
+      document.getElementById("followers").innerHTML = data.followers;
+      document.getElementById("profile").href = data.html_url;
+
+    } catch (error) {
+      console.error("Error fetching GitHub data:", error);
+    }
+  }
 
 
-const url = `https://api.github.com/users/JuggaadSingh`;
-
-fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    
-    document.getElementById("username").innerText = data.login;
-    document.getElementById("repos").innerText = data.public_repos;
-    document.getElementById("followers").innerText = data.followers;
-    document.getElementById("profile-link").href = data.html_url;
-  })
-  .catch(error => console.error("Error fetching GitHub data:", error));
+  getGithubData();
